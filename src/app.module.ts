4b41d75a39c782e6e9daec52240878d2588367a7
Moses Nkrumah <mosessccss@gmail.com>
2021-02-13 11:20:40 +0000

    authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
+import { PasswordEncrypterService } from './authentication/auth-configuration/password-encrypter.service';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { AuthenticationController } from './authentication/authentication.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthenticationModule],
+  controllers: [AuthenticationController, AppController],
+  providers: [PasswordEncrypterService, AppService],
 })
 export class AppModule {}
