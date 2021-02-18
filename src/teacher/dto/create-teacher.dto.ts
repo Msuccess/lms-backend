9af89b174a3f4acf85feb488fd145ad88ba05749
peterdonk <peterdonk17@gmail.com>
2021-02-18 17:04:04 +0000

    Check

--- src/teacher/dto/create-teacher.dto.ts
+++ src/teacher/dto/create-teacher.dto.ts
@@ -4,7 +4,7 @@ import { UserGender } from 'src/shared/enums/gender.enum';
 import { UserRole } from 'src/shared/enums/role.enum';
 
 export class CreateTeacherDto {
-  public readonly teacherID: string;
+  public readonly userId: string;
 
   @ApiProperty()
   @IsNotEmpty({ message: 'Username cannot be null' })
